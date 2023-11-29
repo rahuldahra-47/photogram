2b47c20346fcbe9886388fb15895a4cd04cfb56c
rahuldahra-47 <rahul.dahra59@gmail.com>
2023-11-29 17:03:26 +0530

    sign up implementation added

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Home } from './_root/pages';
 import SignUpForm from './_auth/forms/SignUpForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
+import { Toaster } from "@/components/ui/toaster";
+
 
 
 const App = () => {
@@ -25,6 +27,7 @@ const App = () => {
                 <Route index element={<Home />} />
                </Route> 
             </Routes>
+            <Toaster />
         </main>
     )
 }
--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,12 +1,13 @@
-"use client";
 
 import * as z from "zod"
+import { useToast } from "@/components/ui/use-toast"
+
+import { Link, useNavigate } from 'react-router-dom'
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -14,29 +15,88 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
+import { SignupValidation } from "@/lib/validation";
+import Loader from "@/components/shared/Loader";
+import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutation";
+import { useUserContext } from "@/context/AuthContext";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
 
 const SignUpForm = () => {
+ 
+  const { toast } = useToast();
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const navigate = useNavigate();
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+
+  const {mutateAsync: createUserAccount, isPending: isCreatingAccount} = useCreateUserAccount();
+
+  const {mutateAsync: signInAccount, isPending : isSigningIn} = useSignInAccount();
+
+
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
     defaultValues: {
+      name: '',
       username: "",
+      email: '',
+      password: '',
     },
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+   const newUser = await createUserAccount(values);
+   if(!newUser){ 
+    return   toast({
+      title: "Sign up failed. Please try again.",
+     
+    })
+   }
+   const session = await signInAccount({
+    email: values.email,  
+    password: values.password,
+   })
+   if(!session){ 
+    return   toast({
+      title: "Sign up failed. Please try again.",
+     
+    })
+   }
+   const isLoggedIn = await checkAuthUser();
 
-    console.log(values)
+   if (isLoggedIn) {
+     form.reset();
+
+     navigate("/");
+   } else {
+     toast({ title: "Login failed. Please try again.", });
+     
+     return;
+   }
   }
   return (
-    <div>
+  
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="sm:w-420 flex-center flex-col">
+          <img src="/assets/images/photo.svg" alt="logo" />
+          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+          <p className="text-light-3 small-medium md:base-regular mt-2">To use photogram, Please enter your details</p>
+     
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+              
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <FormField
             control={form.control}
             name="username"
@@ -44,20 +104,55 @@ const SignUpForm = () => {
               <FormItem>
                 <FormLabel>Username</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input type="text" className="shad-input" {...field} />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
+              
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+              
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+              
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">{isCreatingAccount ? (
+          <div className="flex-center gap-2">
+            <Loader />
+          </div>
+          )
+          :"Sign Up"}
+          </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account? 
+            <Link to='/sign-in' className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+          </p>
         </form>
+        </div>
       </Form>
-    </div>
-
   )
 }
 
